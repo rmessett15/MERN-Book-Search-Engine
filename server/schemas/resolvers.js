3af91e7ed07aaf52b031b473f9ce91a6f66ce154
rmessett15 <rmessett15@gmail.com>
2023-07-12 15:42:32 -0500

    added console.log to resolvers to test functionality of queriying a user

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,6 +4,7 @@ const { signToken } = require("../utils/auth")
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
+      console.log(context.user)
       if (context.user) {
         return User.find({ _id: context.user._id });
       }
